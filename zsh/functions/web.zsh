9d0a5d1961249f9d2c3cf16be8c6b180b2f40239
Bartlomiej Kozal <bkzl@me.com>
2013-07-05 09:58:30 +0200

    Ruby > Python

--- zsh/functions/web.zsh
+++ zsh/functions/web.zsh
@@ -1,7 +1,7 @@
 alias b='bundle'
 alias be='bundle exec'
 
-alias httpserver="python -m SimpleHTTPServer 3000"
+alias httpserver="ruby -run -e httpd . -p3000"
 
 function diki {
   open -a WebKit "http://www.diki.pl/slownik-angielskiego/?q=$1"
