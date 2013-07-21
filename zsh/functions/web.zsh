5bc0991552c849a9bf95ac75af80e0c5e8dbc8a1
Bartlomiej Kozal <bkzl@me.com>
2013-07-21 15:29:06 +0200

    n/c

--- zsh/functions/web.zsh
+++ zsh/functions/web.zsh
@@ -4,5 +4,5 @@ alias be='bundle exec'
 alias httpserver="ruby -run -e httpd . -p3000"
 
 function diki {
-  open -a WebKit "http://www.diki.pl/slownik-angielskiego/?q=$1"
+  open -a Safari "http://www.diki.pl/slownik-angielskiego/?q=$1"
 }
