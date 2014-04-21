5ac549c693da360d732f15679992f3fe4eb1cfed
Bartlomiej Kozal <bkzl@me.com>
2014-04-21 17:56:46 +0200

    Remove Rakefile, start working on setup.sh

---
+++ setup.sh
@@ -0,0 +1,54 @@
+# OS X
+
+# Apps
+
+# SSH
+
+# Homebrew
+
+# Packages
+
+# brew install ctags
+# brew install ghi
+# brew install git
+# brew install hub
+# brew install ncdu
+# brew install node
+# brew install pow
+# brew install rbenv
+# brew install rbenv-ctags
+# brew install rbenv-default-gems
+# brew install reattach-to-user-namespace
+# brew install ruby-build
+# brew install the_silver_searcher
+# brew install tig
+# brew install tmux
+# brew install https://raw.github.com/derekprior/tmux-vim-select-pane/master/tmux-vim-select-pane.rb
+# brew install vim
+# brew install zsh-completions
+
+# Dotfiles
+
+# ln -sf ~/code/bkzl/dotfiles/gittemplate ~/.gittemplate
+# ln -sf ~/code/bkzl/dotfiles/vim ~/.vim
+# ln -sf ~/code/bkzl/dotfiles/zsh ~/.zsh
+
+# ln -sf ~/code/bkzl/dotfiles/agignore ~/.agignore
+# ln -sf ~/code/bkzl/dotfiles/ctags ~/.ctags
+# ln -sf ~/code/bkzl/dotfiles/gemrc ~/.gemrc
+# ln -sf ~/code/bkzl/dotfiles/gitconfig ~/.gitconfig
+# ln -sf ~/code/bkzl/dotfiles/gitignore ~/.gitignore
+# ln -sf ~/code/bkzl/dotfiles/irbrc ~/.irbrc
+# ln -sf ~/code/bkzl/dotfiles/tmux.conf ~/.tmux.conf
+# ln -sf ~/code/bkzl/dotfiles/vimrc ~/.vimrc
+# ln -sf ~/code/bkzl/dotfiles/zshrc ~/.zshrc
+
+# ln -sf ~/code/bkzl/dotfiles/rbenv-default-gems ~/.rbenv/default-gems
+
+# Secret
+
+# Vundle
+
+# Vim
+
+# Ruby
