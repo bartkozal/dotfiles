30f81b7046590e80ccba83600534c5e9cb797d8a
Bart Kozal <bkzl@me.com>
2017-08-14 22:22:06 +0200

    Use bold font for prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
 function fish_prompt
-    set_color fc1da7
+    set_color fc1da7 --bold
     echo -n (basename $PWD) ''
-    set_color f5bb3d
+    set_color f5bb3d --bold
     echo -n (git symbolic-ref --short HEAD 2>/dev/null) ''
-    set_color 59ff1a
+    set_color 59ff1a --bold
     echo -n '$ '
 end
