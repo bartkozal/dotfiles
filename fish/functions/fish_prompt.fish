92c31caa265ae62d7b412ad8910314b2a0b1cf57
Bart Kozal <b.kozal@catawiki.nl>
2018-04-17 11:44:27 +0200

    Change color of cwd

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-    set_color blue --bold
+    set_color red --bold
     echo -n (basename $PWD) ''
     set_color yellow --bold
     echo -n (git symbolic-ref --short HEAD 2>/dev/null) ''
