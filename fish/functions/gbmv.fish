680bfa974d696b5b74f1763f880a47b54c649414
Bart Kozal <bkzl@me.com>
2019-01-06 23:16:19 +0100

    Update commands to fish 3.0

--- fish/functions/gbmv.fish
+++ fish/functions/gbmv.fish
@@ -1,6 +1,6 @@
 function gbmv
-  git branch -m $argv[0] $argv[1]
-  git push origin :$argv[0]
-  git push origin $argv[1]
-  git branch --set-upstream-to origin/$argv[0] $argv[1]
+  git branch -m $argv[1] $argv[2]
+  git push origin :$argv[1]
+  git push origin $argv[2]
+  git branch --set-upstream origin/$argv[1] $argv[2]
 end
